8dbc9cb72dfb66b6808bbeedd68e57ccebadc58c
shei254 <chegeeric66@gmail.com>
2024-06-17 00:50:25 +0300

    rules debug injection 2

--- app/Http/Controllers/AwsMarketplaceController.php
+++ app/Http/Controllers/AwsMarketplaceController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use \Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
 use Illuminate\Support\Facades\Validator;
 use PHPUnit\Runner\Exception;
 use Shei\AwsMarketplaceTools\Models\AwsCustomer;
