01e73e9150bba96b7a74074a0ecaf30221ee4278
shei254 <chegeeric66@gmail.com>
2024-06-17 01:57:14 +0300

    debug register post 6

--- app/Http/Controllers/AwsMarketplaceController.php
+++ app/Http/Controllers/AwsMarketplaceController.php
@@ -44,7 +44,7 @@ class AwsMarketplaceController extends Controller
                 throw new \Exception("Could not find your associated aws account. Please set up your account through aws or through the app");
             }
 
-            $awsSubscription = AwsSubscription::where("aws_customer_id", $request->customer_id)->get()->latest();
+            $awsSubscription = AwsSubscription::where("aws_customer_id", $request->customer_id)->latest()->first();
             if (!$awsSubscription) {
                 throw new \Exception("Could not find an active subscription for your account");
             }
