15988e51d29a178c2c7531ebf66747fb0afc4ca6
shei254 <chegeeric66@gmail.com>
2024-06-17 03:05:47 +0300

    make plans page inaccessible debug 3

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,7 +69,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
     ];
 
-    public function awsUser () {
+    public function awsCustomer () {
         return $this->hasOne(AwsCustomer::class, "user_id", "id");
     }
 
